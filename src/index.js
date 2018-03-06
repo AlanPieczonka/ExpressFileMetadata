9d94e920531a47db70b9e742430faf3b14b29b9e
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-06 17:13:54 +0100

    Connect /uploads route with the rest of the app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+
 import indexRoute from './routes/index';
+import uploadRoute from './routes/upload';
 
 const app = express();
 app.use('/', indexRoute);
+app.use('/upload', uploadRoute);
 
 app.listen(8080, () => console.log('Running on port 8080'));
