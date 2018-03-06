b91a71ebce2289568cfa552160ca5794f8b2421f
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-06 17:12:23 +0100

    Create index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 
 const router = express.Router();
-
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html')); 
 });
